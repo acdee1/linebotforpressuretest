467cf4e5818b98840747f612ea96c877bee7b7b4
acdee1 <120545242+acdee1@users.noreply.github.com>
2022-12-22 21:02:16 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,23 @@
+import os
+from linebot import LineBotApi
+from linebot.models import  TextSendMessage,TemplateSendMessage,ButtonsTemplate
+channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+
+def send_text_message(reply_token, text):
+    line_bot_api = LineBotApi(channel_access_token)
+    line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
+    return "OK"
+
+def send_button_message(reply_token, title, text, btn):
+    line_bot_api = LineBotApi(channel_access_token)
+    message = TemplateSendMessage(
+        alt_text='button template',
+        template = ButtonsTemplate(
+            title = title,
+            text = text,
+            actions = btn
+        )
+    )
+    line_bot_api.reply_message(reply_token, message)
+    return "OK"
+
